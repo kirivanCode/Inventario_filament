522aaf1a8e78c5d29f1c3a9f7332ebe3c0eadde3
kirivanCode <ivansierravega8@gmail.com>
2024-10-08 11:25:58 -0500

    final3

--- config/app.php
+++ config/app.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://inventariofilament-production.up.railway.app'),
 
     'asset_url' => env('ASSET_URL'),
 
